9b2829b2ec742a33afa9505cc73ba947f6e1fc1c
imkr099 <imkr099@gmail.com>
2023-02-05 13:37:04 +0600

    'djangohw8'

--- InternetShop/urls.py
+++ InternetShop/urls.py
@@ -15,25 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from products.views import main, products_view, main_view, \
-    product_detail_view, categories_view, create_product_view
+from products.views import MainView, ProductView, ProductDetailView, CategoryView, CreateProductView
 from django.conf.urls.static import static
 from InternetShop.settings import MEDIA_URL, MEDIA_ROOT
-from users.views import login_view, logout_view, register_view
+from users.views import LoginView, LogoutView, RegisterView
 
 urlpatterns = [
-    path('', main),
+    path('', MainView.as_view()),
     path('admin/', admin.site.urls),
-    path('products/', products_view),
-    path('main_view/', main_view),
-    path('products/<int:id>/', product_detail_view),
-    path('categories/', categories_view),
-    path('products/create/', create_product_view),
+    path('products/', ProductView.as_view()),
+    path('products/<int:id>/', ProductDetailView.as_view()),
+    path('categories/', CategoryView.as_view()),
+    path('products/create/', CreateProductView.as_view()),
 
     #users
-    path('users/login/', login_view),
-    path('users/logout/', logout_view),
-    path('users/register/', register_view)
+    path('users/login/', LoginView.as_view()),
+    path('users/logout/', LogoutView.as_view()),
+    path('users/register/', RegisterView.as_view())
 ]
 
 urlpatterns += static(MEDIA_URL, document_root= MEDIA_ROOT)
--- products/forms.py
+++ products/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ProductCreateForm(forms.Form):
+    image = forms.FileField()
     title = forms.CharField(max_length=100)
     description = forms.CharField(widget=forms.Textarea())
     price = forms.FloatField(required=False)
